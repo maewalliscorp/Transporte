8f07c8eeb226b6f39a10adee0d4a465b3b813bc9
LauLau7 <laurabravo0207@gmail.com>
2025-09-26 15:09:57 -0600

    cambio

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
-        <!-- un comentario nuevo aquí -->
+        <!-- un comentario -->
 
 
         <link rel="icon" href="/favicon.ico" sizes="any">
