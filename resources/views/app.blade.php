f6c70f058ed47c672ff144e390318454f0f30fba
LauLau7 <laurabravo0207@gmail.com>
2025-09-27 22:07:55 -0600

    cambio

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,14 +5,13 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
-        <!-- un comentario :) -->
-
-
+        /*
+        Este es un comentario
+        */
         <link rel="icon" href="/favicon.ico" sizes="any">
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml">a
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml">
         <link rel="apple-touch-icon" href="/apple-touch-icon.png">
         <p>Hola</p>
-
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
