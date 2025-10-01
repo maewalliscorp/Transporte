5bd24fcde841f8a85908ac8e482a8556f2eb47d2
LauLau7 <laurabravo0207@gmail.com>
2025-10-01 00:22:32 -0600

    avance

--- routes/auth.php
+++ routes/auth.php
@@ -9,6 +9,8 @@ use App\Http\Controllers\Auth\NewPasswordController;
 use App\Http\Controllers\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
+
+
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
@@ -33,6 +35,7 @@ Route::middleware('guest')->group(function () {
 
     Route::post('reset-password', [NewPasswordController::class, 'store'])
         ->name('password.update');
+
 });
 
 Route::middleware('auth')->group(function () {
